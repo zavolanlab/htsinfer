d05a93b87c46b637b9b3da39ca1791f7de7cf2c5
Rohan Kandhari <rohankan73@gmail.com>
2020-07-27 02:17:45 +0530

    adding biomart script

---
+++ scripts/extract_gene_id.R
@@ -0,0 +1,44 @@
+##---------------------------------------------------------------
+##-- The script extracts Gene ID's of a gene in an organism for all it's orthologues
+##-- Example: Here we are extracting Homo Sapien's most abundant ribosomal 
+##-- protein gene id's for all it's orthologues
+##---------------------------------------------------------------
+
+library(biomaRt)
+ensembl = useEnsembl(biomart = "ensembl", dataset = "hsapiens_gene_ensembl", mirror = "asia")
+
+# List of Genes for which Gene ID's needs to be retrieved for it's orthologues 
+genes = c("RPSA", "RPS2", "RPS3", "RPS3A", "RPS4X", "RPS4Y1", "RPS5", 
+            "RPS6", "RPS7", "RPS8", "RPS9", "RPS10", "RPS11", "RPS12", "RPS13")
+
+## Prerequisite ****************
+# Create an .csv file, before running the script, that will store the desired attributes needed for every organism 
+df = read.csv('attributes.csv', header = FALSE)
+df = sapply(df, as.character)
+##******************************
+
+for (gene in genes){
+    
+    # Creating empty dataframe that will store Gene ID's for all the orthologues for gene variable
+    gene_ids_df = data.frame(matrix(nrow = 0, ncol=7))
+    
+    for (i in 1:nrow(df)){
+
+        # Using getBM() to retrieve Gene ID for every organism in attributes.csv  
+        id = getBM(filters = "external_gene_name",
+                    attributes = c(df[i,"Type"], df[i,"Orthologue"], df[i,"Target %id"], 
+                                    df[i,"Query %id"], df[i,"High Confidence"]),
+                    values = gene,
+                    mart = ensembl)
+        id = unname(x)
+        id = data.frame(df[i,"Species"], id, df[i,"Tax ID"])
+        # print(x)
+        # Merging dataframe for every organism's iteration
+        gene_ids_df = rbind(as.matrix(table), as.matrix(id))
+    }
+
+    colnames(gene_ids_df) = c("Species", "Type", "Orthologue", "Target %id", 
+                                "Query %id", "High Confidence", "Tax ID")
+    file_name = paste(gene, ".csv",sep="")
+    write.csv(gene_ids_df, file=file_name, row.names = FALSE)
+}
---
+++ scripts/extract_gene_transcripts.R
@@ -0,0 +1,79 @@
+##---------------------------------------------------------------
+##-- The script extracts transcripts of a gene in an organism for all it's orthologues
+##-- Example: Here we are extracting Saccharomyces cerevisiae's most abundant ribsomal 
+##-- protein gene transcripts for all it's orthologues
+##---------------------------------------------------------------
+
+library(biomaRt)
+library(seqinr)
+
+# List of Genes for transcripts needs to be retrieved
+genes_fungi = c("RPS0A", "RPS0B", "RPS2", "RPS3", "RPS1A",
+                    "RPS1B", "RPS4A", "RPS4B", "RPS5", "RPS6A",
+                        "RPS6B", "RPS7A", "RPS7B", "RPS8A", "RPS8B")
+number_genes = length(genes_fungi)
+
+# Create .fasta file for each respective gene 
+for (i in 1:number_genes){
+    gene_transcript_file = paste("scerevisiae_", genes_fungi[i], ".fasta", sep = "")
+    cat( "", file = gene_transcript_file, append = FALSE, sep = "")
+}
+
+# Looping for every Gene in genes_fungi
+for (i in 1:number_genes){
+    
+    # Reading .csv file that was created by extract_gene_id script
+    gene_id_file = paste("scerevisiae_", genes_fungi[i], ".csv", sep = "" )
+    df = read.csv(gene_id_file, header = TRUE )
+    df = sapply(df, as.character)
+    
+    # Iterating for every organism in .csv file
+    for(j in 1:nrow(df)){
+
+        organism_name = df[j,"Species"]
+        gene_ensembl_name = df[j,"Orthologue"]
+
+        # If no Gene ID for that organism then skip it 
+        if(is.na(gene_ensembl_name))
+        {
+            next
+        }
+
+        # Here dataset refers to organism's dataset in ensembl mart, example: aclavatus_eg_gene
+        dataset = paste(organism_name, "_eg_gene", sep = "")
+
+        # For other Ensembls like Fungi, Plants, Bacteria
+        ensembl = useMart(host = "https://fungi.ensembl.org", 
+                            biomart = "fungi_mart", dataset = dataset, 
+                            port = 443)
+        
+        # For regular Ensembl Biomart 
+        # ensembl <- useEnsembl(biomart = "ensembl", 
+        #                        dataset = dataset, 
+        #                        mirror = "asia")
+        
+        # Dataframe with Transcript ID and cdna sequence
+        transcript_df = biomaRt::getBM(filters = "ensembl_gene_id",
+                           attributes = c("ensembl_transcript_id", "cdna"),  
+                           values = gene_ensembl_name,
+                           mart = ensembl)
+        
+        gene_transcript_file = paste("scerevisiae_", genes_fungi[i], ".fasta", sep = "")
+        for (k in 1:nrow(transcript_df)){
+            
+            # print(transcript_df[k,1])
+        
+            id = paste(genes_fungi[i], gene_ensembl_name, 
+                        transcript_df[k,1], organism_name, 
+                            df[j,"Tax ID"], sep="|")
+            # Appending transcripts to the gene's fasta file
+            write.fasta(sequences = transcript_df[t,2], names=id, 
+                                file.out = gene_transcript_file, 
+                                open = "a", nbchar = 60, 
+                                as.string = TRUE)
+        
+            }
+        
+    }
+    
+}
