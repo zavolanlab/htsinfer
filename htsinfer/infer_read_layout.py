dcfb613fbd9e490ce6bae57b1eda45bd6e902644
Alex Kanitz <alexander.kanitz@alumni.ethz.ch>
2020-10-21 01:29:35 +0300

    feat: call read layout inference module (#37)

--- htsinfer/infer_read_layout.py
+++ htsinfer/infer_read_layout.py
@@ -1,8 +1,20 @@
 """Infer read layout from sample data."""
 
 
-def infer():
-    """Main function coordinating the execution of all other functions.
-    Should be imported/called from main app and return results to it.
+def infer(
+    file_1: str,  # pylint: disable=unused-argument
+    file_2: str = None,  # pylint: disable=unused-argument
+) -> str:
+    """Infers read layout for single- or paired-ended sequencing libraries in
+    FASTQ format.
+
+    Args:
+        file_1 (str) : File path to read/first mate library.
+        file_2 (str) : File path to second mate library.
+
+    Returns:
+        LIBTYPE string according to Salmon documentation, cf.
+        https://salmon.readthedocs.io/en/latest/library_type.html
     """
-    # implement me
+    # implement logic
+    return "U"
