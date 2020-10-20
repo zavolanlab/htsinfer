3a4e494dd848b8a3f57015d6e823850c3bb4fa7e
Alex Kanitz <alexander.kanitz@alumni.ethz.ch>
2020-10-21 01:53:52 +0300

    feat: add CLI arg to pass organism (#38)

--- htsinfer/infer_read_layout.py
+++ htsinfer/infer_read_layout.py
@@ -1,16 +1,22 @@
 """Infer read layout from sample data."""
 
+from typing import Union
+
 
 def infer(
     file_1: str,  # pylint: disable=unused-argument
     file_2: str = None,  # pylint: disable=unused-argument
+    organism: Union[int, str] = "hsapiens",  # pylint: disable=unused-argument
 ) -> str:
     """Infers read layout for single- or paired-ended sequencing libraries in
     FASTQ format.
 
     Args:
-        file_1 (str) : File path to read/first mate library.
-        file_2 (str) : File path to second mate library.
+        file_1: File path to read/first mate library.
+        file_2: File path to second mate library.
+        organism: Source organism of the sequencing library; either a short
+            name (string, e.g., `hsapiens`) or a taxon identifier (integer,
+            e.g., `9606`).
 
     Returns:
         LIBTYPE string according to Salmon documentation, cf.
