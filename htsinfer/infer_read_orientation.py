b32bf9f6e6bb3526701ce5206b955188539120fc
System Administrator <howhc@live.com>
2020-10-21 00:19:20 +0200

    added some codes in infer_read_orientation.py for issue 33 which is to creater temporary folder for dumping working memories then delete it at the end.

--- htsinfer/infer_read_orientation.py
+++ htsinfer/infer_read_orientation.py
@@ -1,8 +1,44 @@
-"""Infer read orientation from sample data."""
-
-
-def infer():
+"""Infer read orientation from sample data.""" def infer(iputDir):
     """Main function coordinating the execution of all other functions.
     Should be imported/called from main app and return results to it.
     """
     # implement me
+    
+    #create .tmp
+    mkTmpDir(inputDir)
+    
+    #Do something then change directory back to previous directory to allow .tmp to 
+    #be deleted
+    
+    os.chdir(inputDir)
+    print("Changed working directory to %s " % tmpPath)
+    
+    # delete .tmp
+    rmTmpDir()
+    
+    
+def mkTmpDir(inputDir):
+  import os
+  # define the name of the directory to be created
+  tmpPath = inputDir + "/.tmp"
+  
+  try:
+      os.mkdir(tmpPath)
+  except OSError:
+      print ("Creation of the directory %s failed" % tmpPath)
+  else:
+      print ("Successfully created the directory %s " % tmpPath)
+  
+  os.chdir()
+  print("Changed working directory to %s " % tmpPath)
+      
+def rmTmpDir(tmpPath):
+  import os
+  
+  #remove temporary directory
+  try:
+    os.rmdir(tmpPath) 
+except OSError:
+    print ("Deletion of the directory %s failed" % tmpPath)
+  else:
+    print ("Successfully deleted the directory %s" % tmpPath)
