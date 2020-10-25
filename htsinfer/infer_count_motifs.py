ac6da3867d71ebd939f0958d481252d84a6bfe6e
Lukas <lukas.heuberger@gmail.com>
2020-10-26 00:02:04 +0100

    added unittest, renamed module, more documentation

---
+++ htsinfer/infer_count_motifs.py
@@ -0,0 +1,78 @@
+def count_motifs(input_sequences, min_motif_length, max_motif_length, nucleic_acid):
+    """ Function that calculates the occurrence of all possible motifs in one or multiple sequences and
+            returns a dictionary with all motifs within the specified length and their occurrence
+
+        Args:
+            input_sequences (list): list of sequences
+            min_motif_length (int): minimal length of motif
+            max_motif_length (int): maximal length of motif
+            nucleic_acid (string): type of nucleic acid (dna, rna)
+
+        Returns:
+            motif frequency (dict) with paired data {"motif_seq": frequency }
+
+        Raises:
+            TypeError: input_sequences is not a list
+            TypeError: min_motif_length or max_motif_length is not an integer
+            ValueError: nucleic_acid is not in specified range
+            ValueError: max_motif_length is longer than the shortest list
+        """
+
+    if not isinstance(input_sequences, list):
+        raise TypeError('input_sequences is not a list of sequences!')
+
+    if not input_sequences:
+        raise TypeError('input_sequences is not a list of sequences!')
+
+    if not isinstance(min_motif_length, int):
+        raise TypeError('min_motif_length is not an integer!')
+
+    if not isinstance(max_motif_length, int):
+        raise TypeError('max_motif_length is not an integer!')
+
+    if nucleic_acid.lower() not in ['rna', 'dna']:
+        raise ValueError('nucleic_acid is not in specified range (dna/rna)!')
+
+    if max_motif_length > len(min(input_sequences)):
+        raise ValueError('max_motif_length is longer than the shortest list')
+
+    motif_freq = {}
+    for sequence_no in range(0, len(input_sequences)):
+        seq = input_sequences[sequence_no]
+        user_choice = 0
+
+        # determine valid characters depending on input sequences
+        if nucleic_acid.lower() == 'dna':
+            valid_bases = dict.fromkeys('ACTG')
+        if nucleic_acid.lower() == 'rna':
+            valid_bases = dict.fromkeys('ACUG')
+
+        # check if all characters in current sequence are valid. if not, ask user what to do with invalid characters
+        if not all(base in valid_bases for base in seq):
+            print('sequence', sequence_no+1,  'contains unknown characters')
+            print('you have the following choices:')
+            print('(1) ignore the non-nucleotide-characters and exclude them from the motifs')
+            print('(2) include the non-nucleotide characters')
+            print('(3) ignore the sequence with the non-nucleotide characters')
+            user_choice = input('please specify how you want to proceed using the numbers above: ')
+
+        # user choice 3 skips this sequence and continues with the next sequence
+        if user_choice == '3':
+            print('sequence', sequence_no+1, 'ignored')
+            continue
+
+        # collect all motifs and add to dict
+        for motif_length in range(min_motif_length, max_motif_length + 1):
+            for char in range(0, len(seq) - motif_length + 1):
+                motif = seq[char: char + motif_length]
+                if all(char in valid_bases for char in motif):
+                    if motif in motif_freq:
+                        motif_freq[motif] += 1
+                    else:
+                        motif_freq[motif] = 1
+                else:
+                    print('motif not valid: ', motif)
+
+    sorted_dict = dict(
+        sorted(motif_freq.items(), key=lambda item: item[1], reverse=True))  # sorted dict in descending order
+    return sorted_dict
---
+++ tests/test_count_motifs.py
@@ -0,0 +1,84 @@
+"""Unit tests for infer_single_paired.py"""
+
+from htsinfer.infer_count_motifs import count_motifs
+import pytest
+
+test_sequences_DNA = [
+    "CGTACTTGGTTCGATCAACCAGGCCGTAAAGTACGACGTCATCAAAAACGTTTAAACAAA",
+    "GCTCAACGTGTTGCTCCTCGACCAGCCAAAGGTTCATTACGGCCAGTTGTTCGTGGTACC",
+    "GCTGCTGGTCTTCATCCAACCTATGCTCGAACCATCGGTATTTCAGTTGATCATCGACGA",
+    "ACACGTCGTTATAACATGAAAGTACGTTCTGGACGCGGTTTTTCTTTGGATGAAATTCGT"
+    ]
+
+test_sequences_RNA = [
+    "CGUACUUGGUUCGAUCAACCAGGCCGUAAAGUACGACGUCAUCAAAAACGUUUAAACAAA",
+    "GCUCAACGUGUUGCUCCUCGACCAGCCAAAGGUUCAUUACGGCCAGUUGUUCGUGGUACC",
+    "GCUGCUGGUCUUCAUCCAACCUAUGCUCGAACCAUCGGUAUUUCAGUUGAUCAUCGACGA",
+    "ACACGUCGUUAUAACAUGAAAGUACGUUCUGGACGCGGUUUUUCUUUGGAUGAAAUUCGU"
+    ]
+
+test_sequences_invalid = [
+    "CGTAXTTGGTTCGATRAACCAGGCCGTAPAAGTACGACMTCATQCAAAAACGTTNAAACAAA",
+    "GCTGCTGGTCTTCATXCCAACCTATGCTCGAACCATCGGTATTTCAGTTGATWCATCGACGA"
+    ]
+
+test_sequences_one_wrong = [
+    "CGTACTTGGTTCGATCAACCAGGCCGTAAAGTACGACGTCATCAAAAACGTTTAAACAAA",
+    "GCTCAACGTGTTGCTCCTCXGACCAGCCAAAGGTTCATTACGGCCAGTTGTTCGTGGTACC",
+    "GCTGCTGGTCTTCATCCAACCTATGCTCGAACCATCGGTATTTCAGTTGATCATCGACGA",
+    "ACACGTCGTTATAACATGAAAGTACGTTCTGGACGCGGTTTTTCTTTGGATGAAATTCGT"
+    ]
+
+test_not_list = 'GCTCAACGTGTTGCTCCTCGACCAGCCAAAGGTTCATTACGGCCAGTTGTTCGTGGTACC'
+
+test_empty_list = []
+
+test_empty_string = ""
+
+class TestInfer:
+    """Test `count_motifs()` function."""
+
+    def test_dna(self):
+        """Test DNA sequences"""
+        motifs_dict = count_motifs(test_sequences_DNA, min_motif_length=1,max_motif_length=6,nucleic_acid='dna')
+        assert bool(motifs_dict) is True
+
+    def test_rna(self):
+        """Test RNA sequences"""
+        motifs_dict = count_motifs(test_sequences_RNA, min_motif_length=1, max_motif_length=6, nucleic_acid='rna')
+        assert bool(motifs_dict) is True
+
+    def test_invalid_sequence(self):
+        """Test invalid sequences"""
+        with mock.patch('builtins.input', return_value="1"):
+            motifs_dict = count_motifs(test_sequences_invalid, min_motif_length=3, max_motif_length=6, nucleic_acid='dna')
+            assert bool(motifs_dict) is True
+
+        with mock.patch('builtins.input', return_value="2"):
+            motifs_dict = count_motifs(test_sequences_invalid, min_motif_length=3, max_motif_length=6, nucleic_acid='dna')
+            assert bool(motifs_dict) is True
+
+        with mock.patch('builtins.input', return_value="3"):
+            motifs_dict = count_motifs(test_sequences_invalid, min_motif_length=3, max_motif_length=6, nucleic_acid='dna')
+            assert bool(motifs_dict) is False
+
+    def test_partial_invalid_sequence(self):
+        """Test sequence where only one is wrong and will be skipped"""
+        with mock.patch('builtins.input', return_value="3"):
+            motifs_dict = count_motifs(test_sequences_one_wrong, min_motif_length=3, max_motif_length=6, nucleic_acid='dna')
+            assert bool(motifs_dict) is True
+
+    def test_not_list(self):
+        """Test sequence that is not a list"""
+        with pytest.raises(TypeError):
+            assert count_motifs(test_not_list, min_motif_length=3, max_motif_length=9, nucleic_acid='rna')
+
+    def test_empty_list(self):
+        """Test sequence list that is empty"""
+        with pytest.raises(TypeError):
+            assert count_motifs(test_not_list, min_motif_length=3, max_motif_length=9, nucleic_acid='rna')
+
+    def test_empty_string(self):
+        """Test sequence that is an empty string"""
+        with pytest.raises(TypeError):
+            assert count_motifs(test_empty_list, min_motif_length=3, max_motif_length=9, nucleic_acid='rna')
