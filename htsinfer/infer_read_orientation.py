4301500df651293d90364348f7b2b3721a4d806d
System Administrator <howhc@live.com>
2020-11-03 01:26:28 +0100

    Simplified and corrected overall code based on comments by reviewer (uniqueg)

--- htsinfer/infer_read_orientation.py
+++ htsinfer/infer_read_orientation.py
@@ -1,65 +1,39 @@
 """Infer read orientation from sample data."""
 
-from typing import Union
+import logging
+import os
+import shutil
+from tempfile import mkdtemp
 
-def infer(
-    file_1: str,  # pylint: disable=unused-argument
-    file_2: str = None,  # pylint: disable=unused-argument
-    organism: Union[int, str] = "hsapiens",  # pylint: disable=unused-argument
-) -> str:
-    """Infers read orientation for single- or paired-ended sequencing libraries
-    in FASTQ format.
+LOGGER = logging.getLogger(__name__)
 
-    Args:
-        file_1: File path to read/first mate library.
-        file_2: File path to second mate library.
-        organism: Source organism of the sequencing library; either a short
-            name (string, e.g., `hsapiens`) or a taxon identifier (integer,
-            e.g., `9606`).
 
-    Returns:
-        LIBTYPE string according to Salmon documentation, cf.
-        https://salmon.readthedocs.io/en/latest/library_type.html
+def infer():
+    
+    """Creates a temporary directory (tmp_dir) in the input 
+    directory (input_dir). Returns the path of tmp_dir.
+    tmp_dir is used for the storage of temporary data 
+    created when performing the main function. 
+    After completion of main function, tmp_dir is deleted.
     """
-    #create .tmp directory in "inputDir" and retuns .tmp path
-    tmpDir = mkTmpDir(inputDir)
     
-    # call functions of issues #25, 26, 27 and 34
+    #creates temporary directory in the working directory
+    try:
+        tmp_dir = mkdtemp(dir=os.getcwd())
+    except OSError as exc:
+        raise OSError("Creation of temporary directory failed") from exc
+    LOGGER.info(f"Created temporary directory {tmp_dir}.")
     
-    # delete .tmp
-    rmTmpDir(tmpDir)
-        
-    # implement logic
-    return "U"
+    # Main function in between creation and deletion of tmp_dir
+    
+    
+    # delete temporary directory
+    try:
+        shutil.rmtree(tmp_dir)
+    except OSError as exc:
+        raise OSError("Deletion of temporary directory failed") from exc
+    LOGGER.info(f"Deleted temporary directory {tmp_dir}.")
+
+    
     
 
-def mkTmpDir(inputDir):
-    """Function to create a temporary directory for the storage of 
-    temporary data."""
-  
-    import os
-
-    # define the name of the directory to be created
-    tmpPath = inputDir + "/.tmp"
-  
-    try:
-        os.mkdir(tmpPath)
-    except OSError:
-      print ("Creation of the directory %s failed" % tmpPath)
-    else:
-      print ("Successfully created the directory %s " % tmpPath)
-  
-    return (tmpPath)
-      
-      
-def rmTmpDir(tmpPath):
-    """Function to remove the temporary directory."""
-  
-    import os
-    #remove temporary directory
-    try:
-        os.rmdir(tmpPath)
-    except OSError:
-        print ("Deletion of the directory %s failed" % tmpPath)
-    else:
-        print ("Successfully deleted the directory %s" % tmpPath)
