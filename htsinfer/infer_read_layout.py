4ffacdfc09c6520e4b0c64a2df12db046c185ef9
ymoon06 <71296198+ymoon06@users.noreply.github.com>
2020-11-04 20:21:15 +0900

    change
    change

--- htsinfer/infer_read_layout.py
+++ htsinfer/infer_read_layout.py
@@ -5,6 +5,7 @@ def infer():
     """Main function coordinating the execution of all other functions.
     Should be imported/called from main app and return results to it.
     """
+    pass
     # implement me
 
 
