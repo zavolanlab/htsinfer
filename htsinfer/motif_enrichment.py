4554f5a0141696a54a9da56ebfd603fe98beed22
Paul Ritsche <paul.ritsche@unibas.ch>
2020-11-04 09:15:45 +0100

    added motif_enrichment test

--- htsinfer/motif_enrichment.py
+++ htsinfer/motif_enrichment.py
@@ -1,17 +1,16 @@
 """Calculate motif enrichment from sample data."""
 
-from timeit import timeit
+import numpy as np
 import scipy
 from scipy.stats.distributions import binom
-import sys
 
 
-foreground = {"TGATTC": 5, "TAAACC": 3, "AAGTTACCT": 1, "AAGCCTT": 1, "AGTTCTA": 0, "TTTCCCG": 5}
-background = {"TGATTC": 3, "TAAACC": 5, "AAGCCTTAT": 0, "AGTTCTA": 1, "TTTCCCG": 5, "TTGGAA": 7}
+foreground = {"UGAUUC": 5, "UAAACC": 3, "AAGUUACCU": 1, "AAGCCUU": 1, "AGUUCUA": 1, "UUUCCCG": 5}
+background = {"UGAUUC": 3, "UAAACC": 5, "AAGCCUUAU": 1, "AGUUCUA": 1, "UUUCCCG": 5, "UUGGAA": 7}
 
 
 def motifEnrichment(
-    foreground, 
+    foreground,
     background
 ): 
     """Calculates enrichment and p-values of motifs with similar lengths.
@@ -24,21 +23,30 @@ def motifEnrichment(
         Dictionary including motifs, sum of counts of motifs with 
         similar lenght, enrichment score and p-value
 
-    Raises:
-        .
+    Example: 
+        foreground = {"TGATTC": 5, "TAAACC": 3, "AAGTTACCT": 1, "AAGCCTT": 1, "AGTTCTA": 1, "TTTCCCG": 5}
+        background = {"TGATTC": 3, "TAAACC": 5, "AAGCCTTAT": 1, "AGTTCTA": 1, "TTTCCCG": 5, "TTGGAA": 7}
+
+        >>> motifEnrichment(foreground, background)
+            {'TGATTC': [0.7333333333333334, 0.01646090534979423],
+            'TAAACC': [0.7333333333333334, 0.21947873799725662],
+            'AAGTTACCT': [1.375, 0.26337448559670795],
+            'AAGCCTT': [1.6041666666666667, 0.26337448559670795],
+            'AGTTCTA': [1.6041666666666667, 0.26337448559670795],
+            'TTTCCCG': [1.6041666666666667, 0.01646090534979423]}
     """ 
    
      # Foreground dictionary
     newDictForeground = dict()
     newDictForegroundProb = dict()
-    newDictFinalF = dict()
+    newDictForegroundFinal = dict()
+    newDictForegroundBinom = dict()
     keyListF = list(foreground.keys())
     lengthListF = list()
     
     # Background dictionary
     newDictBackground = dict()
     newDictBackgroundProb = dict()
-    newDictFinalB= dict()
     keyListB = list(background.keys())
     lengthListB = list()
     
@@ -47,53 +55,56 @@ def motifEnrichment(
     for i in keyListF: 
         
         if len(i) in lengthListF:
-            newDictForeground[len(i)] += 1      
+            newDictForeground[len(i)] += foreground[i]     
         else: 
-            newDictForeground[len(i)] = 1
+            newDictForeground[len(i)] = foreground[i]
             lengthListF.append(len(i))
             
     # Calculates probability of occurrence of motifs
     for i in newDictForeground:
         
-        probOfOccurrenceF = newDictForeground[i] / len(foreground)
+        probOfOccurrenceF = newDictForeground[i] / sum(foreground.values())
         newDictForegroundProb[i] = probOfOccurrenceF
-      
+    
     # Background dictionary
     # Calulates sum of motifs of same length
     for i in keyListB: 
         
         if len(i) in lengthListB:
-            newDictBackground[len(i)] += 1      
+            newDictBackground[len(i)] += background[i]     
         else: 
-            newDictBackground[len(i)] = 1
+            newDictBackground[len(i)] = background[i]
             lengthListB.append(len(i))
-            
+    
     # Calculates probability of occurrence of motifs
     for i in newDictBackground:
         
-        probOfOccurrenceF = newDictBackground[i] / len(background)
-        newDictBackgroundProb[i] = probOfOccurrenceF
+        probOfOccurrenceF = newDictBackground[i] / sum(background.values())
+        newDictBackgroundProb[i] = probOfOccurrenceF    
     
-        
+    # Main
     # Calculates enrichment 
     enrichmentDict = dict()
     
     for i in newDictForegroundProb:
 
-        enrichmentDict[i] = newDictForegroundProb[i] / newDictBackgroundProb[i]
+        enrichmentDict[i] = newDictForegroundProb[i] / newDictBackgroundProb[i]    
     
-    # Adds enrichment values
+    # Calculate p-value for foreground motifs    
+    n = len(foreground)
+    p = np.average(list(newDictBackgroundProb.values()))
         
+    for i in foreground:
+        
+        r = foreground[i]
+        newDictForegroundBinom[i] = binom.pmf(r, n, p)
+       
+    # Create dictionary containing enrichment and p-values 
     for i in foreground: 
 
-        newDictFinalF[i] = enrichmentDict[len(i)]
+        newDictForegroundFinal[i] = [enrichmentDict[len(i)], newDictForegroundBinom[i]]
     
-    for i in background: 
+    return(newDictForegroundFinal)
 
-        newDictFinalB[i] = enrichmentDict[len(i)]
-        
-    print(newDictFinalF)
-    print(newDictFinalB)
 
 motifEnrichment(foreground, background)
-
---
+++ tests/test_motif_enrichment.py
@@ -0,0 +1,39 @@
+import pytest
+#from htsinfer.htsinfer import (
+#    motif_enrichment
+#)
+
+
+# Test parameters
+foreground = {"UGAUUC": 5, "UAAACC": 3, "AAGUUACCU": 1, "AAGCCUU": 1, "AGUUCUA": 1, "UUUCCCG": 5}
+background = {"UGAUUC": 3, "UAAACC": 5, "AAGCCUUAU": 1, "AGUUCUA": 1, "UUUCCCG": 5, "UUGGAA": 7}
+foreground_str = {"UGAUUC": "test", "UAAACC": 3, "AAGUUACCU": 1, "AAGCCUU": 1, "AGUUCUA": 1, "UUUCCCG": 5}
+final_dict = {'TGATTC': [0.7333333333333334, 0.01646090534979423],
+            'TAAACC': [0.7333333333333334, 0.21947873799725662],
+            'AAGTTACCT': [1.375, 0.26337448559670795],
+            'AAGCCTT': [1.6041666666666667, 0.26337448559670795],
+            'AGTTCTA': [1.6041666666666667, 0.26337448559670795],
+            'TTTCCCG': [1.6041666666666667, 0.01646090534979423]}
+
+
+class TestMotifEnrichment:
+    """Tests for 'motif_enrichment' function."""
+
+    def test_no_dictionaries(self):
+        "No arguments passed."
+        with pytest.raises(TypeError):
+            motif_enrichment()
+
+    def test_one_dictionary(self):
+        "Missing one required argument."
+        with pytest.raises(TypeError): 
+            motif_enrichment(foreground)
+
+    def test_str_as_value(self):
+        "Unsupported operand type 'str' as value in dictionary."
+        with pytest.raises(TypeError):
+            motif_enrichment(foreground_str, background)
+
+    def test_valid_dictionaries(self): 
+        "Valid dictionaries passed."
+        assert motif_enrichment(foreground, background) == final_dict
