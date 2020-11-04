150bb5e0bcc97c5312329bce9360f35fbeefe5bd
buffalo0124 <jihoon.han24@gmail.com>
2020-11-04 02:35:57 +0100

    Update htsinfer_potential_kmer.v1.py

--- htsinfer_potential_kmer.v1.py
+++ htsinfer_potential_kmer.v1.py
@@ -13,16 +13,16 @@ def kmer_similarity(kmer_input: list, kmer_size: int >= 5 or <= 20) -> Dict:
     """Function that returns set of kmers
     
         Args:
-            kmer_input (list) : list of k-mers
-            kmer_size (integer) : length of k-mers, minimum 5 and maximum 20
+            kmer_input (list)   :   list of k-mers
+            kmer_size (integer) :   length of k-mers, minimum 5 and maximum 20
                     
         Returns:
             set of kmers
             
         Raises:
-            TypeError:  'input_kmers' is not a list
-            TypeError:  'kmer_size' is not a integer
-            ValueError: 'kmer_size' value is invalid
+            TypeError           :   'input_kmers' is not a list
+            TypeError           :   'kmer_size' is not a integer
+            ValueError          :   'kmer_size' value is invalid
    
     """
 
