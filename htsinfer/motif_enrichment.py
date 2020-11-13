ccda7b3cc845f4361e4cb08325784b3e13782ee6
Paul Ritsche <paul.ritsche@unibas.ch>
2020-11-13 09:48:11 +0100

    fixed: pytest & linting errors

--- htsinfer/motif_enrichment.py
+++ htsinfer/motif_enrichment.py
@@ -1,18 +1,14 @@
 """Calculate motif enrichment from sample data."""
 
-import numpy as np
+
 from scipy.stats.distributions import binom
 from typing import (Dict, List)
-
-FOREGROUND_DICT = {"UGAUUC": 5, "UAAACC": 3, "AAGUUACCU": 2,
-                   "AAGCCUU": 1, "AGUUCUA": 1, "UUUCCCG": 5}
-BACKGROUND_DICT = {"UGAUUC": 3, "UAAACC": 5, "AAGCCUUAU": 1,
-                   "AGUUCUA": 1, "UUUCCCG": 5, "UUGGAA": 7}
 
 
 def motif_enrichment(
     foreground: Dict[str, int],
-    background: Dict[str, int]) -> None: 
+    background: Dict[str, int]
+) -> Dict:
     """Calculates enrichment and p-values of motifs with similar lengths.
 
     Args:
@@ -37,20 +33,18 @@ def motif_enrichment(
             'AAGCCUUAU': [0.26666666666666666, 0.984375],
             'UUGGAA': [0.20454545454545456, 0.9847514510038162]}
     """
-    
+
     # Foreground dictionary
     pseudocode_motifs_foreground: Dict = dict()
     new_dict_foreground: Dict = dict()
-    new_dict_foreground_prob: Dict = dict()
     new_dict_foreground_final: Dict = dict()
     length_list_f: List = list()
 
     # Background dictionary
     pseudocode_motifs_background: Dict = dict()
     new_dict_background: Dict = dict()
-    new_dict_background_prob: Dict = dict()
     length_list_b: List = list()
-    
+
     # Create same motifs in foreground and background dictionaries
     # Add foreground motifs to pseudocode dictionary
     for i in foreground:
@@ -60,14 +54,14 @@ def motif_enrichment(
         else:
             pseudocode_motifs_foreground[i] = foreground[i]
             # Increase counts by amount of motfs added
-            for b in background: 
+            for b in background:
                 if len(b) == len(i):
                     background[b] += 1
-            for b in foreground: 
-                if len(b) == len(i): 
+            for b in foreground:
+                if len(b) == len(i):
                     foreground[b] += 1
             pseudocode_motifs_foreground[i] = 1
-            
+
     # Add background motifs to pseudocode dictionary
     for i in background:
 
@@ -76,20 +70,20 @@ def motif_enrichment(
         else:
             pseudocode_motifs_background[i] = background[i]
             # Increase counts by amount of motifs added
-            for f in foreground: 
+            for f in foreground:
                 if len(f) == len(i):
                     foreground[f] += 1
-            for f in background: 
+            for f in background:
                 if len(f) == len(i):
                     background[f] += 1
             pseudocode_motifs_background[i] = 1
-            
+
     # Merge pseudocode and background
-    for i in pseudocode_motifs_foreground: 
+    for i in pseudocode_motifs_foreground:
         background[i] = pseudocode_motifs_foreground[i]
-    
+
     # Merge pseudocode and foreground
-    for i in pseudocode_motifs_background: 
+    for i in pseudocode_motifs_background:
         foreground[i] = pseudocode_motifs_background[i]
 
     # Foreground dictionary
@@ -111,26 +105,23 @@ def motif_enrichment(
         else:
             new_dict_background[len(i)] = background[i]
             length_list_b.append(len(i))
-       
+
     # Main
     # Calculates enrichment
     enrichment_dict: Dict = dict()
 
-    for i in foreground: 
+    for i in foreground:
 
-        enrichment_dict[i] = ((foreground[i] * new_dict_background[len(i)]) / 
-                              (background[i] * new_dict_foreground[len(i)]))    
+        enrichment_dict[i] = ((foreground[i] * new_dict_background[len(i)]) /
+                              (background[i] * new_dict_foreground[len(i)]))
 
     # Calculate p-value for foreground motifs
     # Create dictionary containing enrichment and p-values
     for i in foreground:
-        r_binom = foreground[i]
-        n_binom = new_dict_foreground[len(i)]
-        p_binom = background[i] / new_dict_background[len(i)]
+        r_bin = foreground[i]
+        n_bin = new_dict_foreground[len(i)]
+        p_bin = background[i] / new_dict_background[len(i)]
         new_dict_foreground_final[i] = [enrichment_dict[i],
-                                        (1 - binom.cdf(r_binom, n_binom, p_binom))]
+                                        (1 - binom.cdf(r_bin, n_bin, p_bin))]
 
     return new_dict_foreground_final
-
-
-
--- tests/test_motif_enrichment.py
+++ tests/test_motif_enrichment.py
@@ -1,23 +1,22 @@
 """Unit tests for motif_enrichment.py"""
 
 import pytest
-from typing import Dict
 from htsinfer.motif_enrichment import motif_enrichment
 
 FOREGROUND_DICT = {"UGAUUC": 5, "UAAACC": 3, "AAGUUACCU": 2,
                    "AAGCCUU": 1, "AGUUCUA": 1, "UUUCCCG": 5}
 BACKGROUND_DICT = {"UGAUUC": 3, "UAAACC": 5, "AAGCCUUAU": 1,
                    "AGUUCUA": 1, "UUUCCCG": 5, "UUGGAA": 7}
-FOREGROUND_STR: Dict = {"UGAUUC": "test", "UAAACC": 3, "AAGUUACCU": 1, "AAGCCUU": 1, "AGUUCUA": 1,
-                  "UUUCCCG": 5}
-FINAL_DICT: Dict = {'UGAUUC': [2.4545454545454546, 0.0037400679729227004],
-            'UAAACC': [1.0909090909090908, 0.28899727345086257],
-            'AAGUUACCU': [3.2, 0.0009765625],
-            'AAGCCUU': [1.8, 0.09059653269912638],
-            'AGUUCUA': [0.9, 0.3899216801618356],
-            'UUUCCCG': [0.9, 0.5592643397856017],
-            'AAGCCUUAU': [0.26666666666666666, 0.984375],
-            'UUGGAA': [0.20454545454545456, 0.9847514510038162]}
+FOREGROUND_STR = {"UGAUUC": "test", "UAAACC": 3, "AAGUUACCU": 1,
+                    "AAGCCUU": 1, "AGUUCUA": 1, "UUUCCCG": 5}
+FINAL_DICT = {'UGAUUC': [2.4545454545454546, 0.0037400679729227004],
+                'UAAACC': [1.0909090909090908, 0.28899727345086257],
+                'AAGUUACCU': [3.2, 0.0009765625],
+                'AAGCCUU': [1.8, 0.09059653269912638],
+                'AGUUCUA': [0.9, 0.3899216801618356],
+                'UUUCCCG': [0.9, 0.5592643397856017],
+                'AAGCCUUAU': [0.26666666666666666, 0.984375],
+                'UUGGAA': [0.20454545454545456, 0.9847514510038162]}
 
 
 class Test_motif_enrichment:
