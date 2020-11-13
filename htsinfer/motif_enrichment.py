3c70c2eb8267836f9655bcad2a22d697dd6e533e
Paul Ritsche <paul.ritsche@unibas.ch>
2020-11-13 13:33:45 +0100

    rechecked: ready to push

--- htsinfer/motif_enrichment.py
+++ htsinfer/motif_enrichment.py
@@ -1,13 +1,108 @@
 """Calculate motif enrichment from sample data."""
 
-
-from scipy.stats.distributions import binom
 from typing import (Dict, List)
+from scipy.stats import binom
+
+
+def motif_adding_foreground(
+        foreground: Dict[str, int],
+        background: Dict[str, int]
+) -> Dict:
+    """Creates dictionary with motifs included in foreground but
+        not background. Increases count of motifs with similar lengths
+        in foreground by one for each motif missing in background.
+        Adds pseudo-code of 1 to each motif added to background.
+        Increases count of motifs with similar lengths
+        in background by one for each added motif.
+
+    Args:
+        foreground: dictionary of motifs with counts
+        background: dictionary of motifs with counts
+
+    Returns:
+        Dictionary with missing motifs in background.
+
+    Example:
+        foreground = {"UGAUUC": 5, "CCUUAA": 3, "AAGUUACCU": 2,
+                      "AAGCCUU": 1, "AGUUCUA": 1, "UUUCCCG": 5}
+        background = {"UGAUUC": 9, "UAAACC": 1, "AAGUUACCU": 3,
+                      "AAGCCUU": 1, "AGUUCUA": 4, "UUUAAG": 3}
+        >>> motif_adding(foreground, background)
+           {'CCUUAA': 1, 'UUUCCCG': 1}
+    """
+    # Pseudocode dictionary foreground
+    pseudocode_motifs_foreground: Dict = dict()
+
+    # Add foreground motifs to pseudocode dictionary
+    for i in foreground:
+
+        if i in background:
+            pass
+        else:
+            pseudocode_motifs_foreground[i] = foreground[i]
+            # Increase counts by amount of motfs added
+            for value in background:
+                if len(value) == len(i):
+                    background[value] += 1
+            for value in foreground:
+                if len(value) == len(i):
+                    foreground[value] += 1
+            pseudocode_motifs_foreground[i] = 1
+
+    return pseudocode_motifs_foreground
+
+
+def motif_adding_background(
+        foreground: Dict[str, int],
+        background: Dict[str, int]
+) -> Dict:
+    """Creates dictionary with motifs included in background but
+        not foreground. Increases count of motifs with similar lengths
+        in background by one for each motif missing in foreground.
+        Adds pseudo-code of 1 to each motif added to foreground.
+        Increases count of motifs with similar lengths
+        in foreground by one for each added motif.
+
+    Args:
+        foreground: dictionary of motifs with counts
+        background: dictionary of motifs with counts
+
+    Returns:
+        Dictionary with motifs missing in foreground.
+
+    Example:
+        foreground = {"UGAUUC": 5, "UAAACC": 3, "AAGUUACCU": 2,
+                      "AAGCCUU": 1, "AGUUCUA": 1, "UUUCCCG": 5}
+        background = {"UGAUUC": 9, "UAAACC": 1, "AAGUUACCU": 3,
+                      "AAGCCUU": 1, "AGUUCUA": 4, "UUUCCCG": 3}
+        >>> motif_adding_background(foreground, background)
+            {'UAAACC': 1, 'UUUAAG': 1}
+    """
+    # Pseudocode dictionary background
+    pseudocode_motifs_background: Dict = dict()
+
+    # Add background motifs to pseudocode dictionary
+    for i in background:
+
+        if i in foreground:
+            pass
+        else:
+            pseudocode_motifs_background[i] = background[i]
+            # Increase counts by amount of motifs added
+            for value in foreground:
+                if len(value) == len(i):
+                    foreground[value] += 1
+            for value in background:
+                if len(value) == len(i):
+                    background[value] += 1
+            pseudocode_motifs_background[i] = 1
+
+    return pseudocode_motifs_background
 
 
 def motif_enrichment(
-    foreground: Dict[str, int],
-    background: Dict[str, int]
+        foreground: Dict[str, int],
+        background: Dict[str, int]
 ) -> Dict:
     """Calculates enrichment and p-values of motifs with similar lengths.
 
@@ -21,70 +116,35 @@ def motif_enrichment(
     Example:
         foreground = {"UGAUUC": 5, "UAAACC": 3, "AAGUUACCU": 2,
                       "AAGCCUU": 1, "AGUUCUA": 1, "UUUCCCG": 5}
-        background = {"UGAUUC": 3, "UAAACC": 5, "AAGCCUUAU": 1,
-                      "AGUUCUA": 1, "UUUCCCG": 5, "UUGGAA": 7}
+        background = {"UGAUUC": 9, "UAAACC": 1, "AAGUUACCU": 3,
+                      "AAGCCUU": 1, "AGUUCUA": 4, "UUUCCCG": 3}
         >>> motif_enrichment(foreground, background)
-            {'UGAUUC': [2.4545454545454546, 0.0037400679729227004],
-            'UAAACC': [1.0909090909090908, 0.28899727345086257],
-            'AAGUUACCU': [3.2, 0.0009765625],
-            'AAGCCUU': [1.8, 0.09059653269912638],
-            'AGUUCUA': [0.9, 0.3899216801618356],
-            'UUUCCCG': [0.9, 0.5592643397856017],
-            'AAGCCUUAU': [0.26666666666666666, 0.984375],
-            'UUGGAA': [0.20454545454545456, 0.9847514510038162]}
+            {'UGAUUC': [0.6944444444444444, 0.96190821],
+             'UAAACC': [3.75, 0.005024350000000011],
+             'AAGUUACCU': [1.0, 0.0],
+             'AAGCCUU': [1.1428571428571428, 0.21460819244384766],
+             'AGUUCUA': [0.2857142857142857, 0.9375],
+             'UUUCCCG': [1.9047619047619047, 0.013209342956542969]}
     """
+    # Create same motifs in both dictionaries
+    pseudo_foreground = motif_adding_foreground(foreground, background)
+    pseudo_background = motif_adding_background(foreground, background)
+
+    # Merge pseudocode and dictionaries
+    for i in pseudo_background:
+        foreground[i] = pseudo_background[i]
+
+    for i in pseudo_foreground:
+        background[i] = pseudo_foreground[i]
 
     # Foreground dictionary
-    pseudocode_motifs_foreground: Dict = dict()
     new_dict_foreground: Dict = dict()
     new_dict_foreground_final: Dict = dict()
     length_list_f: List = list()
 
     # Background dictionary
-    pseudocode_motifs_background: Dict = dict()
     new_dict_background: Dict = dict()
     length_list_b: List = list()
-
-    # Create same motifs in foreground and background dictionaries
-    # Add foreground motifs to pseudocode dictionary
-    for i in foreground:
-
-        if i in background:
-            pass
-        else:
-            pseudocode_motifs_foreground[i] = foreground[i]
-            # Increase counts by amount of motfs added
-            for b in background:
-                if len(b) == len(i):
-                    background[b] += 1
-            for b in foreground:
-                if len(b) == len(i):
-                    foreground[b] += 1
-            pseudocode_motifs_foreground[i] = 1
-
-    # Add background motifs to pseudocode dictionary
-    for i in background:
-
-        if i in foreground:
-            pass
-        else:
-            pseudocode_motifs_background[i] = background[i]
-            # Increase counts by amount of motifs added
-            for f in foreground:
-                if len(f) == len(i):
-                    foreground[f] += 1
-            for f in background:
-                if len(f) == len(i):
-                    background[f] += 1
-            pseudocode_motifs_background[i] = 1
-
-    # Merge pseudocode and background
-    for i in pseudocode_motifs_foreground:
-        background[i] = pseudocode_motifs_foreground[i]
-
-    # Merge pseudocode and foreground
-    for i in pseudocode_motifs_background:
-        foreground[i] = pseudocode_motifs_background[i]
 
     # Foreground dictionary
     # Calulates sum of motifs of same length
@@ -118,10 +178,10 @@ def motif_enrichment(
     # Calculate p-value for foreground motifs
     # Create dictionary containing enrichment and p-values
     for i in foreground:
-        r_bin = foreground[i]
         n_bin = new_dict_foreground[len(i)]
         p_bin = background[i] / new_dict_background[len(i)]
         new_dict_foreground_final[i] = [enrichment_dict[i],
-                                        (1 - binom.cdf(r_bin, n_bin, p_bin))]
+                                        (1 - binom.cdf(foreground[i],
+                                                       n_bin, p_bin))]
 
     return new_dict_foreground_final
