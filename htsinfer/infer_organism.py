be8c6c573d75a1607aaeaa894b3ba7925d0b6e02
rohank63 <rohankan73@gmail.com>
2021-02-10 22:54:14 +0530

    bugs fixed

--- htsinfer/infer_organism.py
+++ htsinfer/infer_organism.py
@@ -3,14 +3,22 @@
 import os
 import pandas as pd
 import subprocess as sp
+import operator
+import logging
+from typing import (Dict, Tuple)
+
+logger = logging.getLogger(__name__)
 
 
-def kallisto(file_1: str, file_2: str = None):
+def kallisto(file_1: str, file_2: str = None) -> Dict[Tuple[str, str], float]:
     """Builds index file and run quantification algorithm.
 
     Args:
         file_1 (str) : File path to read/first mate library.
         file_2 (str) : File path to second mate library.
+
+    Returns:
+        A dictionary of count percentage information for top five organisms.
     """
     index = "kallisto index -i transcripts.idx --make-unique transcripts.fasta"
     quant_single = "kallisto quant -i transcripts.idx -o output -l 100 -s 300 --single " + file_1
@@ -19,29 +27,36 @@ def kallisto(file_1: str, file_2: str = None):
 
     try:
         with open(os.devnull, "w") as fnull:
+            logger.debug("Running Kallisto index")
             sp.call(index, shell=True)
+            logger.debug("Running Kallisto quant")
             if file_2 is not None:
                 sp.call(quant_paired, shell=True)
             else:
                 sp.call(quant_single, shell=True)
     except sp.CalledProcessError:
+        logger.error("Invalid input file")
         raise Exception("Error running kallisto")
 
+    logger.debug("Processing organism count info")
+    result = process_count_info()
+    return result
 
-def count_info():
+
+def process_count_info() -> Dict[Tuple[str, str], float]:
     """Infers organisms count and return them as dictionary.
 
     Returns:
-        A Dictionary with count percentage for every organism.
-        example : ('scerevisiae', '4932'): 89.22159777956873
+        A Dictionary with count percentage for organisms.
+        example : ('scerevisiae', '4932'): 89.22
     """
     # Reading tsv file created by kallisto quant
     path = os.path.dirname(os.path.abspath(__file__))
-    file_path = os.path.join(path, "output\\abundance.tsv")
+    file_path = os.path.join(path, "output", "abundance.tsv")
     df = pd.read_csv(file_path, sep='\t')
 
     # Dictionary to store organism info
-    organism_count: Dict[(str, int), float] = {}
+    organism_tpm_count: Dict[(str, int), float] = {}
     dimension = df.shape
     rows = dimension[0]
     total_tpm = 0.0
@@ -50,17 +65,20 @@ def count_info():
         contents = list(map(str, row_id.split("|")))
         organism_name = contents[3]
         organism_tax_id = contents[4]
-        # Update organism count
-        if (organism_name, organism_tax_id) in organism_count:
-            organism_count[(organism_name, organism_tax_id)] += float(df['tpm'][i])
+        # Update organism tpm count
+        if (organism_name, organism_tax_id) in organism_tpm_count:
+            organism_tpm_count[(organism_name, organism_tax_id)] += float(df['tpm'][i])
         else:
-            organism_count[(organism_name, organism_tax_id)] = float(df['tpm'][i])
+            organism_tpm_count[(organism_name, organism_tax_id)] = float(df['tpm'][i])
         total_tpm += float(df['tpm'][i])
 
-    # Sorting as per organism with the highest counts
-    sorted_organism_count = {k: v for k, v in sorted(organism_count.items(), key=lambda item: -1*item[1])}
-
     # Calculating Percentage
-    for i in sorted_organism_count:
-        sorted_organism_count[i] = (sorted_organism_count[i]/total_tpm)*100
-    return sorted_organism_count
+    for i in organism_tpm_count:
+        organism_tpm_count[i] = round((organism_tpm_count[i]/total_tpm)*100, 2)
+
+    # Sorting as per organism with the highest counts
+    # sorted_organism_count = {k: v for k, v in sorted(organism_count.items(), key=lambda item: -1*item[1])}
+
+    # Returning top five organisms as per TPM counts
+    top_five = dict(sorted(organism_tpm_count.items(), key=operator.itemgetter(1), reverse=True)[:5])
+    return top_five
