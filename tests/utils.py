59224414a56c34de11aa3cb81ccf8484fedb79ba
Rohan Kandhari <33807249+rohank63@users.noreply.github.com>
2022-10-06 19:00:39 +0530

    test: add unit tests for library source inference (#90)

--- tests/utils.py
+++ tests/utils.py
@@ -28,6 +28,7 @@ FILE_INCONSISTENT_IDS_SINGLE_MATE = (
 FILE_INCONSISTENT_IDS_SINGLE_OLD_NEW = (
     TEST_FILES_DIR / "inconsistent_single_old_new.fastq"
 )
+FILE_INVALID_PATH = TEST_FILES_DIR / 'invalid'
 FILE_INVALID_SEQ_1 = TEST_FILES_DIR / "invalid_seq_1.fastq"
 FILE_INVALID_SEQ_2 = TEST_FILES_DIR / "invalid_seq_2.fastq"
 FILE_MATE_1 = TEST_FILES_DIR / "first_mate.fastq"
@@ -43,9 +44,10 @@ FILE_ORIENTATION_SR = TEST_FILES_DIR / "orientation_sr.fastq"
 FILE_ORIENTATION_U = TEST_FILES_DIR / "orientation_u.fastq"
 FILE_2000_RECORDS = TEST_FILES_DIR / "2000_records.fastq"
 FILE_SINGLE = TEST_FILES_DIR / "single.fastq"
+FILE_SOURCE_FRUIT_FLY = TEST_FILES_DIR / "fruit_fly.fastq"
 FILE_SRA_SAMPLE_1 = TEST_FILES_DIR / "sra_sample_1.fastq"
 FILE_SRA_SAMPLE_2 = TEST_FILES_DIR / "sra_sample_2.fastq"
-FILE_TRANSCRIPTS = TEST_FILES_DIR / "transcripts.fa"
+FILE_TRANSCRIPTS = TEST_FILES_DIR / "transcripts.fasta"
 FILE_TRANSCRIPTS_GZ = TEST_FILES_DIR / "transcripts.fa.gz"
 FILE_UNKNOWN_SEQ_ID = TEST_FILES_DIR / "unknown_seq_id.fastq"
 FILE_UNMAPPED_PAIRED_1 = TEST_FILES_DIR / "unmapped_paired_mate_1.fastq"
@@ -58,6 +60,7 @@ DICT_DF = {
     'key_1': 2,
     'key_2': 3,
 }
+SOURCE_FRUIT_FLY = Source(short_name="dmelanogaster", taxon_id=7227)
 SOURCE_HUMAN = Source(short_name="hsapiens", taxon_id=9606)
 SEQ_ID_DUMMY = ""
 SEQ_ID_MATE_1 = "@SRR11971718:6:73:941:1973#0/1"
@@ -67,6 +70,7 @@ SEQ_ID_SINGLE = "@SRR11971718:6:73:941:1973#0"
 
 # helper classes
 class RaiseFileProblem:
+    """Raise ``FileProblem`` ."""
     def __init__(self, *args, **kwargs):
         raise FileProblem
 
@@ -84,5 +88,6 @@ class RaiseOSError:
 
 
 class RaiseValueError:
+    """Raise ``ValueError``."""
     def __init__(self, *args, **kwargs):
         raise ValueError
