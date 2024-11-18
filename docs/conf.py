f231a5422017b63e2964dd6027bf7b0be4c958ec
balajtimate <balajtimate@gmail.com>
2024-11-18 19:53:35 +0100

    update conf exclusions

--- docs/conf.py
+++ docs/conf.py
@@ -48,9 +48,7 @@ templates_path = ['_templates']
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = [
-    'setup.py',
-    '.DS_Store',
-    '.eggs'
+    'setup.py'
 ]
 
 # Default doc to search for
